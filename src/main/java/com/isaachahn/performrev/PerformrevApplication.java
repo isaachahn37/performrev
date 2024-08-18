b4f4d7fc92e7686c79a222dfddb4a2e33efb9fa2
isaachahnepicareer <isaac@epicareer.com>
2024-08-18 16:18:44 +0700

    init project

---
+++ src/main/java/com/isaachahn/performrev/PerformrevApplication.java
@@ -0,0 +1,13 @@
+package com.isaachahn.performrev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformrevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformrevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isaachahn/performrev/PerformrevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaachahn.performrev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformrevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
